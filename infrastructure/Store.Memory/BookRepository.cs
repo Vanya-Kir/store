7739816d39623f87af88ca5356fcfd619a4fda62
Ivan Kirillov <van-kirillov@yandex.ru>
2022-07-27 22:31:39 +0300

    add: добавить BookRepository

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.Memory;
+
+public class BookRepository : IBookRepository
+{
+    private readonly Book[] books = new[]
+    {
+        new Book(1, "Art of programming"),
+        new Book(2, "Refactoring"),
+        new Book(3, "Clean Code"),
+    };
+    public Book[] GetAllByTitle(string titlePart)
+    {
+        return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+    }
+}
